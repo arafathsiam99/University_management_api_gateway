5978ab00074a523cb75fbd485d05892f76da564b
unknown <siammdarafathislam@gmail.com>
2023-11-25 22:10:23 +0600

    image upload updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { FileUploadHelper } from '../../../helpers/fileUploadHelper';
+import { UserService } from './user.service';
+import sendResponse from '../../../shared/response';
 
 const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.file, req.data);
-  //   FileUploadHelper.uploadToCloudinary();
+  try {
+    const result = await UserService.createStudent(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const UserController = {
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,19 +2,21 @@ import express, { NextFunction, Request, Response } from 'express';
 import { UserController } from './user.controller';
 import auth from '../../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../../enums/user';
-import { UserValidation } from './user.validation';
-import { FileUploadHelper } from '../../../helpers/fileUploadHelper';
+import { FileUploadHelper } from '../../../helpers/FileUploadHelper';
+import { UserValidation } from './user.validations';
 
 const router = express.Router();
 
 router.post(
-  '/create-student',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-  FileUploadHelper.upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = UserValidation.createStudent.parse(JSON.parse(req.body.data));
-    return UserController.createStudent(req, res, next);
-  }
+    '/create-student',
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+    FileUploadHelper.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserValidation.createStudent.parse(JSON.parse(req.body.data))
+        return UserController.createStudent(req, res, next)
+    }
 );
+
+
 
 export const userRoutes = router;
